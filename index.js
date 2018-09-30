e54fe975863337695cb078aa99d0bfc09974f727
Tr4sh P4nda <thetr4shp4nda@gmail.com>
2018-09-30 17:26:38 -0500

    Initial Commit

--- index.js
+++ index.js
@@ -1,38 +1,68 @@
-//Link to invite SamBot with required permissions - https://discordapp.com/api/oauth2/authorize?client_id=494477057867251745&scope=bot&permissions=3537984
+// //Link to invite SamBot with required permissions - https://discordapp.com/api/oauth2/authorize?client_id=494477057867251745&scope=bot&permissions=3537984
 
-const Commando = require('discord.js-commando');
+const Discord = require('discord.js-commando');
 const Music = require('discord.js-musicbot-addon');
-const MultiGuildPlayer = require('discord-playlist/discord-playlist').MultiGuildPlayer;
-const bot = new Commando.Client();
-const TOKEN = 'NDk0NDc3MDU3ODY3MjUxNzQ1.Do0GQA.Cfa9WQYcFQqif2jB7ZxJKnt2rTY';
-//const ytdl = require('ytdl-core');
+// const MultiGuildPlayer = require('discord-playlist/discord-playlist').MultiGuildPlayer;
+const config = require('./config.json');
+const client = new Discord.Client();
 
-bot.registry.registerGroup('simple', 'Simple');
-bot.registry.registerDefaults();
-bot.registry.registerCommandsIn(__dirname + '/commands/');
+// This allows Admin to customize cmd prefix (!prefix [Your Custom Prefix])
+// client.on("message", async message => {
+//   if(message.author.client) return;
+//   if(message.channel.type === "dm") return;
 
-global.servers = {};
+//   let prefixes = JSON.parse(fs.readFileSynv("./prefixes.json", "utf8"));
 
-bot.on('message', function(message){
-  if(message.content == 'Hello'){
-    message.channel.sendMessage('Hello!');
+//   if(!prefixes[mesage.guild.id]){
+//     prefixes[message.guild.id] = {
+//       prefixes: botconfig.prefix
+//     };
+//   }
+
+//   let prefix = prefixes[mesasge.guild.id].prefixes;
+//   console.log(prefix);
+// });
+
+const cPrefix = "s!";
+
+client.on("message", (message) => {
+
+  if (!message.content.startsWith(cPrefix) || message.author.bot) return;
+
+  if (message.content.startsWith(cPrefix + "ping")) {
+
+    message.channel.send("pong!");
+
+  } else
+
+  if (message.content.startsWith(cPrefix + "foo")) {
+
+    message.channel.send("bar!");
+
   }
+
 });
 
-Music.start(bot, {
-  youtubeKey: 'AIzaSyDNoH8g3hVzyQs_qWiwtwlFLj6Xv1Dt_dc',
+client.registry.registerGroup('simple', 'Simple');
+client.registry.registerDefaults();
+client.registry.registerCommandsIn(__dirname + '/commands/');
+
+Music.start(client, {
+  youtubeKey: config.youtubeKey,
   enableQueueStat: true,
-  prefix: 'm!',
-  //clearInvoker: 'true',
+  prefix: 's!',
+  // clearInvoker: true,
   maxQueueSize: '200',
   embedColor: 'Yellow',
-  anyoneCanLeave: false,
+  anyoneCanLeave: true,
   requesterName: true,
-  //disableLeave: true,
+  // disableLeave: true,
   clearOnLeave: true,
-  botAdmin: ['Tr4sh P4nda#3195']
+  npCmd: 'nowplaying'
 });
 
-//bot.login(TOKEN);
+// console.log('');
 
-bot.on('ready', () => console.log('SamBot is Ready!'));
+client.login(config.token);
+
+client.on('ready', () => console.log('SamBot is Ready!'));
